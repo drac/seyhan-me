876fe86a004d5ddacc8cd7abe2e09383f0b4c05b
drac <seyhan.drc@gmail.com>
2023-12-26 09:55:56 +0200

    feat(app): Add contact page component

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+export default function Contact() {
+  return (
+    <section>
+      <h1>{`<Contact/>`}</h1>
+      <ul>
+        <li><a href="https://github.com/drac">GitHub</a></li>
+        <li><a href="https://www.linkedin.com/in/seyhandzhamur/">LinkedIn</a></li>
+        <li><a href="mailto:contact@seyhan.me">contact@seyhan.me</a></li>
+      </ul>
+    </section>
+  )
+}
