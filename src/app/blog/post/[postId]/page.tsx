464624ff05cb59892052aa11f9313c91bdc69409
drac <seyhan.drc@gmail.com>
2024-01-06 15:50:15 +0200

    feat(blog): Add Error Handling and Markdown Rendering
    - Implement Error component for client-side error handling in blog posts
    - Use `ReactMarkdown` for rendering markdown content in BlogPost
    - Add `getPostData` utility to fetch and parse markdown blog posts
    - Use `gray-matter` for metadata parsing in blog posts
    - Add `getAllPostIds` utility for extracting blog post IDs
    - Enhance `BlogPost` component to display post titles and content
    - Add useEffect in Error component to log errors to a reporting service

--- src/app/blog/post/[postId]/page.tsx
+++ src/app/blog/post/[postId]/page.tsx
@@ -1,9 +1,20 @@
+import { getPostData } from "./utils"
+import ReactMarkdown from 'react-markdown';
 
-
-export default function BlogPost() {
-  return (
-    <section>
-      Test
-    </section>
-  )
+/**
+ * Renders a post based on the provided postId.
+ * @param {Object} props - The component props.
+ * @param {Object} props.params - The parameters object.
+ * @param {string} props.params.postId - The ID of the post.
+ * @returns {JSX.Element} The rendered post component.
+ */
+export default function Post({ params }: { params: { postId: string } }): JSX.Element {
+  const postId = params.postId
+  const post = getPostData(postId)
+  return <section>
+    <h1 style={{ marginBottom: "32px" }}>{post.title}</h1>
+    <ReactMarkdown>
+      {post.content}
+    </ReactMarkdown>
+  </section>
 }
